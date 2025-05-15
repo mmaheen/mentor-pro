cf6d5bf4fe85f2a31210aa16f7daa55abe0030a5
mmaheen <mushrifmaheen@gmail.com>
2025-05-16 03:38:40 +0600

    Header nav item active

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -7,9 +7,9 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="{{ route('index') }}" class="nav-item nav-link active">Home</a>
+            <a href="{{ route('index') }}" class="nav-item nav-link {{ Route::currentRouteName() == 'index' ? 'active':''}}">Home</a>
             <a href="about.html" class="nav-item nav-link">About</a>
-            <a href="{{ route('courses') }}" class="nav-item nav-link">Courses</a>
+            <a href="{{ route('courses') }}" class="nav-item nav-link {{ Route::currentRouteName() == 'courses' ? 'active':''}}">Courses</a>
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                 <div class="dropdown-menu bg-light m-0">
