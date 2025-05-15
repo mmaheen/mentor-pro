ac2a561c952bf3a597c6d3cbe95397eb1711739a
mmaheen <mushrifmaheen@gmail.com>
2025-05-16 03:43:05 +0600

    Header dropdown

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -11,13 +11,10 @@
             <a href="about.html" class="nav-item nav-link">About</a>
             <a href="{{ route('courses') }}" class="nav-item nav-link {{ Route::currentRouteName() == 'courses' ? 'active':''}}">Courses</a>
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                <a href="#" class="nav-link dropdown-toggle {{ Route::currentRouteName() == 'login' || Route::currentRouteName() == 'register' ? 'active':''}}" data-bs-toggle="dropdown">Authentication</a>
                 <div class="dropdown-menu bg-light m-0">
-                    <a href="feature.html" class="dropdown-item">Features</a>
-                    <a href="appointment.html" class="dropdown-item">Appointment</a>
-                    <a href="team.html" class="dropdown-item">Our Team</a>
-                    <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                    <a href="404.html" class="dropdown-item">404 Page</a>
+                    <a href="{{ route('login') }}" class="dropdown-item">Login</a>
+                    <a href="{{ route('register') }}" class="dropdown-item">Register</a>
                 </div>
             </div>
             <a href="contact.html" class="nav-item nav-link">Contact</a>
