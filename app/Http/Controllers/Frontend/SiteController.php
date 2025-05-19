a42b5b18608448a701ce0805b902d7467255055e
mmaheen <mushrifmaheen@gmail.com>
2025-05-19 11:04:19 +0600

    Making Environment and 1st view

---
+++ app/Http/Controllers/Frontend/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    //
+    Public function index()
+    {
+        return view('frontend.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\Frontend\SiteController;
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/',[SiteController::class, 'index'])->name('index');
