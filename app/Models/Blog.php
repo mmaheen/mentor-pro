2a5caa05d169ff65be84ed3ebf66626656fbc3e4
mmaheen <mushrifmaheen@gmail.com>
2025-05-23 08:10:42 +0600

    Relations

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
     //
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
