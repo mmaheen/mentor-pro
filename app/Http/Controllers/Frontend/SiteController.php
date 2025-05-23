09530e7c11162f1ae20d63779fbe3d84475584dc
mmaheen <mushrifmaheen@gmail.com>
2025-05-23 08:18:42 +0600

    Fetch blog

--- app/Http/Controllers/Frontend/SiteController.php
+++ app/Http/Controllers/Frontend/SiteController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SiteController extends Controller
 {
     Public function index()
     {
-        return view('frontend.index');
+        $blogs = Blog::with('category','user')->latest()->paginate(5);
+        return view('frontend.index', compact('blogs'));
     }
 
     public function blogs()
